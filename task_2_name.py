e8a8ed864175f1e8d0e1c75859d531839ee0beb0
J-Greber <josefine.greber@hotmail.com>
2017-10-07 15:41:13 +0200

    update_game

--- task_2_name.py
+++ task_2_name.py
@@ -24,7 +24,7 @@ if door == "1":
 	if answer == "1":
 		print("Congratulations, %s, you found a new friend!" % (name))
 	elif answer == "2":
-		print("Sorry, %s, the vampire is faster. You become a dinner." % (name))
+		print("Sorry, %s, the vampire is faster. You become her dinner." % (name))
 	else:
 		wrong_input()
 
@@ -39,7 +39,18 @@ elif door == "2":
 	if answer == "1":
 		print("You discover that the box is filled with gold and jewels. Congratulations, %s you are now rich." % (name))
 	elif answer == "2":
-		print("There is a skeleton behind the door. You scream and run away.")
+		print("There is a skeleton behind the door.")
+		print("What do you do, %s?" % (name))
+		print("1. You scream and run away.")
+		print("2. You stay to learn more about it.")
+
+		answer = input (">")
+
+		if answer == "1":
+			print("Du Angsthase, %s! Game over." % (name))
+		elif answer == "2":
+			print("The skeleton wakes up and says: Hi, %s. How are you?" % (name))
+		else:wrong_input()
 	else:
 		wrong_input()
 else:
